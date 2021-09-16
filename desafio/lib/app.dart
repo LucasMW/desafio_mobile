ce2f228ca70d955d516c7ad1b9d586c4855182b8
LucasMW <great-lucas@hotmail.com>
2021-09-16 11:37:50 +0100

    added basic login logic

--- desafio/lib/app.dart
+++ desafio/lib/app.dart
@@ -1,20 +1,45 @@
 import 'package:desafio/login_screen.dart';
-import 'package:desafio/map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Desafio',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
+      home: Scaffold(
+        body: FutureBuilder(
+            future: _initialization,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return SafeArea(
+                  child: Container(
+                    color: Colors.red,
+                    child: Text(snapshot.error.toString()),
+                  ),
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                var materialApp = MaterialApp(
+                  title: 'Desafio',
+                  theme: ThemeData(
+                    primarySwatch: Colors.indigo,
+                  ),
+                  home: LoginScreen(),
+                );
+                return materialApp;
+              }
+              return const CircularProgressIndicator();
+            }),
       ),
-      home: const LoginScreen(),
     );
   }
 }
--- desafio/lib/login_screen.dart
+++ desafio/lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,8 +9,32 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    checkLogin();
+    super.initState();
+  }
+
+  void checkLogin() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+    FirebaseAuth.instance.idTokenChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
   TextStyle style = const TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   final emailField = TextField(
+    controller: TextEditingController(),
     //style: style,
     decoration: InputDecoration(
         contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -17,13 +42,46 @@ class _LoginScreenState extends State<LoginScreen> {
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
   );
   final passwordField = TextField(
+    controller: TextEditingController(),
     obscureText: true,
     //style: style,
     decoration: InputDecoration(
-        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         hintText: "Password",
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
   );
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      final token = await userCredential.user!.getIdToken();
+      print("token $token");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<void> register() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: "barry.allen@example.com",
+              password: "SuperSecretPassword!");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +93,10 @@ class _LoginScreenState extends State<LoginScreen> {
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
-          print("Login Pressed");
+          final email = emailField.controller?.text.trim() ?? "none";
+          final password = passwordField.controller?.text.trim() ?? "none";
+          print("Will signin with $email, $password");
+          signIn(email, password);
         },
         child: Text("Login",
             textAlign: TextAlign.center,
@@ -45,18 +106,23 @@ class _LoginScreenState extends State<LoginScreen> {
     );
 
     return SafeArea(
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("LoginScreen"),
-            SizedBox(height: 100),
-            emailField,
-            SizedBox(height: 10),
-            passwordField,
-            SizedBox(height: 50),
-            loginButon
-          ],
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("LoginScreen"),
+                const SizedBox(height: 100),
+                emailField,
+                const SizedBox(height: 10),
+                passwordField,
+                const SizedBox(height: 50),
+                loginButon
+              ],
+            ),
+          ),
         ),
       ),
     );
